c65832012bbed6c2da08c8e608a1505224185c0f
HassanAbbas107 <qiqbx223@gmail.com>
2025-07-09 11:41:15 +0300

    duck

--- scripit/app.js
+++ scripit/app.js
@@ -1,12 +1,18 @@
 function init(){
     const gridElm = document.querySelector('.grid')
+    const cells=[]
+    const girdWidth=10
+    const numOfCells=10*girdWidth
 function createGrid() {
     
     for (let i = 0; i < 100; i++) {
         const cell =document.createElement('div')
         cell.textContent=i
+        cell.classList.add('duck')
+        cells.push(cell)
         gridElm.appendChild(cell)
     }
+    console.log(cells)
 }
 
 createGrid()
