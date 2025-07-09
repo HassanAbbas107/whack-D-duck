109e8e0a7f4bcf99258f6eb3124c38eb7acbfab8
HassanAbbas107 <qiqbx223@gmail.com>
2025-07-09 11:14:41 +0300

    create grid

---
+++ scripit/app.js
@@ -0,0 +1,16 @@
+function init(){
+    const gridElm = document.querySelector('.grid')
+function createGrid() {
+    
+    for (let i = 0; i < 100; i++) {
+        const cell =document.createElement('div')
+        cell.textContent=i
+        gridElm.appendChild(cell)
+    }
+}
+
+createGrid()
+
+
+}
+document.addEventListener("DOMContentLoaded",init)
