9e14778beabc15b31798aed55df88fd6fa4f41ba
HassanAbbas107 <qiqbx223@gmail.com>
2025-07-09 13:57:11 +0300

    sss

--- scripit/app.js
+++ scripit/app.js
@@ -1,39 +1,56 @@
-function init(){
-    const gridElm = document.querySelector('.grid')
-    const cells=[]
-    const gridWidth=10
-    const numOfCells=gridWidth*gridWidth
-    let duckPosition=0;
+function init() {
+    const gridElm = document.querySelector('.grid');
+    score_disp=document.querySelector("#score_disp")
+    const cells = [];
+    const gridWidth = 10;
+    const numOfCells = gridWidth * gridWidth;
+    let duckPosition = 0; 
+    let score=0
     
     function addDuck() {
-        cells[duckPosition].classList.add("Duck")
-    }
-    
-    function removeDuck(){
-        cells[duckPosition].classList.remove("Duck")
-    }
-    
-    function play(){
-        setInterval(() => {
-            removeDuck()
-            duckPosition = Math.floor(Math.random() * cells.length)
-            addDuck()
-        }, 3000)
-    }
-    
-    function createGrid(){
-        for (let i = 0; i < numOfCells; i++) {
-            const cell = document.createElement('div')
-            cell.textContent = i
-            cells.push(cell)
-            gridElm.appendChild(cell)
+        if (cells[duckPosition]) { 
+            cells[duckPosition].classList.add("duck"); 
         }
-        console.log(cells)
     }
     
-    createGrid()
-    addDuck()
-    play()
+    function removeDuck() {
+        if (cells[duckPosition]) { 
+            cells[duckPosition].classList.remove("duck");
+        }
+    }
+    
+    function play() {
+        setInterval(() => {
+            removeDuck();
+            duckPosition = Math.floor(Math.random() * numOfCells); 
+          addDuck();
+        }, 2000); 
+    }
+    
+    function createGrid() {
+        gridElm.style.display = 'grid';
+        gridElm.style.gridTemplateColumns = `repeat(${gridWidth}, 1fr)`; 
+        
+        for (let i = 0; i < numOfCells; i++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell'); 
+            cell.textContent = i;
+            cell.addEventListener('click',handelClick)
+            cells.push(cell);
+            gridElm.appendChild(cell);
+        }
+    }
+    function handelClick(){
+        if(event.target.classList.contains('duck')){
+            score=+100
+            score_disp.textContent=`Your Score is ${score}`
+            
+        }
+    }
+    createGrid();
+    duckPosition = Math.floor(Math.random() * numOfCells); 
+    addDuck();
+    play();
 }
 
-document.addEventListener("DOMContentLoaded", init)
+document.addEventListener("DOMContentLoaded", init);
